3de458584bb10ed28db6b73149182baf5551af2f
Raghottamvkalkeri <raghottam.kalkeri@avetoconsulting.com>
2025-02-21 10:49:40 +0530

    flask

--- Zomato-Data.py
+++ Zomato-Data.py
@@ -1,11 +1,13 @@
 
 #Added Flask to make this csv to API 
 from flask import Flask, jsonify
+from flask_cors import CORS
 import csv
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/APIS', methods=['GET'])
+
 def get_data():
     data_list = []
 
@@ -18,7 +20,11 @@ def get_data():
 
         for index,  row in enumerate(datavalue , start = 1):
             # print("Sl No ", index , "Name " , row[name] , "Ordred Online " ,row[orderdata])
-            data_list.append({})
+            # data_list.append({row})
+            row_data = {"SL No": index}  # Add serial number
+            row_data.update({header[i]: row[i] for i in range(len(header))})  # Map headers to values
+
+            data_list.append(row_data)
 
     return jsonify(data_list)
 
